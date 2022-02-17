860d34641389b21d9dae44acae017ae931e5a122
Tim.Wang <18701799670@163.com>
2022-02-17 15:03:42 +0800

    Added transaction data send

--- transact.py
+++ transact.py
@@ -8,21 +8,39 @@
 # 时分秒
 
 import numpy as np
+import pandas as pd
+from calc_factors import fake_factor
 
 Transaction_Cost = 0.00164
+open_time = 93100
+morning_close = 113000
+afternoon_open = 130000
+close_time = 145700
+
+def int_time_to_tuple(s):
+    second = s % 100
+    s = s // 100
+    minute = s % 100
+    hour = s // 100
+    return [hour, minute, second]
+
+def second_diff(t1, t2):
+    h1, m1, s1 = int_time_to_tuple(t1)
+    h2, m2, s2 = int_time_to_tuple(t2)
+    return (h2 - h1) * 3600 + (m2 - m1) * 60 + (s2 - s1)
 
 class Order():
     '''
     Handle the status of Order
     '''
-    def __init__(self, direction, number, price) -> None:
+    def __init__(self, direction, number, price, send_time) -> None:
         self.position_cost = None
         self.original_number = number
         self.original_price = price
 
         self.current_position = 0
 
-        # 0: Buy 1: Sell
+        # 1: Buy -1: Sell
         self.direction = direction
 
         # 0 : Order Submitted
@@ -33,9 +51,20 @@ class Order():
 
         self.pnl = 0
 
-    def fill(self, price):
-        self.status = 1
-        self.price = price
+        self.send_time = send_time
+
+    def __str__(self):
+        s = []
+
+        s.append("Order sent at: {}".format(self.send_time))
+        s.append("Original order: {}, ￥{}".format(self.original_number, self.original_price))
+        s.append("Cost: {}".format(self.position_cost))
+        s.append("Direction: {}".format({1:'Buy', -1:"Sell"}[self.direction]))
+        s.append("Current status:{}".format({0:'Submitted', 1:'Filled', 2:'Reverse Submitted', 3:'Closed', -1:'Cancelled'}[self.status]))
+        s.append("Current_position: {}".format(self.current_position))
+        s.append("Current_pnl: {}".format(self.pnl))
+
+        return "\n".join(s)
 
 class Account():
     '''
@@ -48,7 +77,6 @@ class Account():
         self.total_pos = 0
 
         self.order_book = []
-        self.order_active = []
 
         self.fee = np.log(Transaction_Cost + 1)
         self.threshold = 0.0
@@ -58,7 +86,11 @@ class Account():
 
         self.daily_limit = None
 
-    def can_send(self, signal, bid1, ask1):
+    def can_send(self, signal, market):
+        bid1 = market['pb1']
+        ask1 = market['pa1']
+        send_time = market['time']
+
         current_pos = self.current_pos
         max_up = min(self.per_order, self.poslimit - current_pos)
         if max_up < 0:
@@ -76,17 +108,40 @@ class Account():
                 max_down = -remain_limit
 
         if signal > (self.fee + self.threshold):
-            return Order(number=max_up, direction=1, price=ask1)
+            if np.isnan(ask1) or ask1 == 0.0:
+                return
+            return Order(number=max_up, direction=1, price=ask1, send_time=send_time)
         elif signal < -(self.fee + self.threshold):
-            return Order(number=-max_down, direction=-1, price=bid1)
+            if np.isnan(bid1) or bid1 == 0.0:
+                return
+            return Order(number=-max_down, direction=-1, price=bid1, send_time=send_time)
 
     def update_pnl(self, price):
         pass
 
     # handle if buy and sell at the same time
-    def will_exaust(self, signal, order):
-        for order in self.order_active:
-            pass
+    def will_exaust(self, signal, order, current_time):
+        if current_time >= 145600:
+            for order in self.order_book:
+                if order.status == 1:
+                    order.status = 2
+
+        for order in self.order_book:
+            if order.status != 1:
+                continue
+            if second_diff(order.send_time, current_time) < 60:
+                continue
+            if order.direction == 1:
+                if signal > self.threshold:
+                    order.send_time = current_time
+                else:
+                    order.status = 2
+                continue
+            if order.direction == -1:
+                if signal < -self.threshold:
+                    order.send_time = current_time
+                else:
+                    order.status = 2
 
 class Exchange():
     def __init__(self):
@@ -105,7 +160,9 @@ class Exchange():
                 trade_cost = 0
                 for i in range(1,6):
                     price = "pa{}".format(i)
-                    quantity = "qa{}".format(i)
+                    quantity = "va{}".format(i)
+                    if np.isnan(price) or price==0.0 or np.isnan(quantity) or quantity==0.0:
+                        continue
 
                     if price > order.original_price:
                         break
@@ -116,6 +173,7 @@ class Exchange():
                     trade_cost += quantity_left * price
                 order.current_position += trade_qty
                 order.pnl -= trade_cost
+                order.position_cost = trade_cost
                 if order.current_position != 0:
                     order.status = 1
                 else:
@@ -126,8 +184,10 @@ class Exchange():
                 trade_qty = 0
                 trade_cost = 0
                 for i in range(1,6):
-                    price = market["pa{}".format(i)]
-                    quantity = market["qa{}".format(i)]
+                    price = market["pb{}".format(i)]
+                    quantity = market["vb{}".format(i)]
+                    if np.isnan(price) or price==0.0 or np.isnan(quantity) or quantity==0.0:
+                        continue
 
                     if price < order.original_price:
                         break
@@ -138,6 +198,7 @@ class Exchange():
                     trade_cost += quantity_left * price
                 order.current_position -= trade_qty
                 order.pnl += trade_cost
+                order.position_cost = trade_cost
                 if order.current_position != 0:
                     order.status = 1
                 else:
@@ -150,7 +211,9 @@ class Exchange():
                 trade_cost = 0
                 for i in range(1,6):
                     price = market["pa{}".format(i)]
-                    quantity = market["qa{}".format(i)]
+                    quantity = market["va{}".format(i)]
+                    if np.isnan(price) or price==0.0 or np.isnan(quantity) or quantity==0.0:
+                        continue
 
                     qty_left = min(-order.current_position - trade_qty, quantity)
                     if qty_left <= 0:
@@ -167,8 +230,10 @@ class Exchange():
                 trade_qty = 0
                 trade_cost = 0
                 for i in range(1,6):
-                    price = market["pa{}".format(i)]
-                    quantity = market["qa{}".format(i)]
+                    price = market["pb{}".format(i)]
+                    quantity = market["vb{}".format(i)]
+                    if np.isnan(price) or price==0.0 or np.isnan(quantity) or quantity==0.0:
+                        continue
 
                     qty_left = min(order.current - trade_qty, quantity)
                     if qty_left <= 0:
@@ -181,8 +246,48 @@ class Exchange():
                     order.status = 3
 
 
+def parse_date(s):
+    year = s[:4]
+    month = s[5:7]
+    day = s[8:10]
+    date = year + month + day
+    return int(date)
+
+def parse_time(s):
+    hour = s[11:13]
+    minute = s[14:16]
+    second = s[17:19]
+    t = hour + minute + second
+    return int(t)
 
 if __name__ == "__main__":
-    pass
+    full_data = pd.read_csv("../star_tick/202101/688399.csv")
+    full_data['date'] = full_data['trade_time'].apply(lambda x : parse_date(x))
+    full_data['time'] = full_data['trade_time'].apply(lambda x : parse_time(x))
+    full_data['log_price'] = np.log(full_data['last_price'])
 
+    all_days = set(full_data['date'])
+    for day in all_days:
+        dta = full_data[full_data['date'] == day].copy()
+        dta['ret'] = dta['log_price'] - dta['log_price'].shift(-20)
 
+        dta = dta[((dta['time'] > open_time) & (dta['time'] < morning_close)) | ((dta['time'] > afternoon_open) & (dta['time'] < close_time))].copy()
+        dta['signal'] = fake_factor(dta['ret'], 0.3)
+        dta['minute'] = dta['time'] // 100
+
+        print(dta)
+
+        first_second = dta.groupby(['minute']).first()['trade_time'].copy()
+        first_second = pd.DataFrame(first_second, columns=['trade_time'])
+        first_second['is_first'] = 1
+        dta = dta.merge(first_second, on='trade_time', how='left')
+
+        account = Account()
+        exchange = Exchange()
+        for _, market in dta.iterrows():
+            if market['is_first'] == 1:
+                order = account.can_send(market['signal'], market)
+                if order is None:
+                    continue
+                print(order)
+        break
